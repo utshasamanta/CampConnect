faf70edea01ed42aa76c288083700faa856cccc0
utshasamanta <utshalm@gmail.com>
2024-07-27 20:20:12 +0600

    fixed JOI validation before multer upload

--- middleware.js
+++ middleware.js
@@ -1,8 +1,7 @@
 const ExpressError = require("./utils/ExpressError");
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const Campground = require("./models/campground");
-const Review = require('./models/review');
-
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -13,13 +12,12 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 };
 
-
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
     }
     next();
-}
+};
 
 module.exports.validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -30,31 +28,40 @@ module.exports.validateCampground = (req, res, next) => {
     next();
 }
 
+//This one runs in the filefilter to ensure body is valid before uploading image
+module.exports.validateCampgroundMulter = (body) => {
+    const { error } = campgroundSchema.validate(body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    }
+};
+
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
+        req.flash("error", "You do not have permission to do that!");
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
-}
+};
 
 module.exports.validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
-        const msg = error.details.map(el => el.message).join(',');
+        const msg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(msg, 400);
     }
     next();
-}
+};
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
+        req.flash("error", "You do not have permission to do that!");
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
-}
+};
--- routes/campground.js
+++ routes/campground.js
@@ -2,11 +2,27 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds.js')
 const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
 const multer  = require('multer')
 const { storage } = require('../cloudinary/index.js');
-const upload = multer({ storage })
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware.js');
+const { isLoggedIn, isAuthor, validateCampground, validateCampgroundMulter } = require('../middleware.js');
+const upload = multer({ 
+    storage,
+    fileFilter: function (req, file, cb) {
+        try {
+            validateCampgroundMulter(req.body) // doing this to validate the fields first if any image uploaded
+        } catch (err) {
+            return cb(err, false);
+        }
 
+        const alloweTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+        if (!alloweTypes.includes(file.mimetype)) {
+           console.log("hello!");
+           return cb(new ExpressError("Only takes png, jpg, jpeg", 400), false);
+        }
+        cb(null, true);
+    }
+})
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
@@ -20,7 +36,7 @@ router.route('/:id')
     .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.editCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get("/:id/edit", isLoggedIn, catchAsync(campgrounds.renderEditForm));
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 
 module.exports = router;
